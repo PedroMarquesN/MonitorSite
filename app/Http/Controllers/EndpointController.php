d8fec750958abbd6b1535aa0323306d4545adf7c
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-01-19 15:43:24 -0300

    add endpoints with error in next check

--- app/Http/Controllers/EndpointController.php
+++ app/Http/Controllers/EndpointController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUpdateEndpointRequest;
+use App\Models\Endpoint;
 use App\Models\Site;
 use Illuminate\Http\Request;
 
@@ -15,6 +17,47 @@ class EndpointController extends Controller
         }
         $endpoints = $site->endpoints;
         
-        return view('admin.endpoints', compact('site', 'endpoints'));
+        return view('admin.endpoints.index', compact('site', 'endpoints'));
+    }
+
+    public function create(string $siteId)
+    {
+        if(!$site = Site::find($siteId)){
+            return back();
+        }
+        return view('admin.endpoints.create', compact('site'));
+    }
+
+    public function store(StoreUpdateEndpointRequest $request , Site $site)
+    {
+
+        $site->endpoints()->create($request->all());
+       
+
+        return redirect()
+                ->route('endpoints.index', $site->id)
+                ->with('message','Cadastro com Sucesso');
+    }
+
+    public function edit(Site $site, Endpoint $endpoint)
+    {
+        return view('admin.endpoints.edit', compact('site', 'endpoint'));
+    }
+
+    public function update(StoreUpdateEndpointRequest $request, Site $site, Endpoint $endpoint)
+    {
+        $endpoint->update($request->validated());
+        return redirect()
+        ->route('endpoints.index', $site->id)
+        ->with('message','Atualizado com Sucesso');
+    }
+    
+    public function destroy(Site $site, Endpoint $endpoint){
+
+        $endpoint->delete();
+
+        return redirect()
+        ->route('endpoints.index', $site->id)
+        ->with('message','Deletado com Sucesso');
     }
 }
--- app/Models/Site.php
+++ app/Models/Site.php
@@ -2,7 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Database\Eloquent\Builder;
+
+
+
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
