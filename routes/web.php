453b047a2deda77aee50208b3eb260c5649ef6e5
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-01-19 20:02:26 -0300

    add kernel and checkcontroller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CheckController;
 use App\Http\Controllers\Admin\SiteController;
 use App\Http\Controllers\EndpointController;
 use App\Http\Controllers\ProfileController;
@@ -23,6 +24,8 @@ Route::get('job', function(){
 });
 
 Route::middleware(['auth'])->prefix('admin')->group(function (){
+
+    Route::get('/endpoints/{endpoint}/logs',[CheckController::class, 'index'])->name('endpoints.checks');
 
     Route::resource('/sites/{site}/endpoints', EndpointController::class);
     
